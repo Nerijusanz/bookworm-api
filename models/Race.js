a27ed0b8d6ea1b641a93d678bff1e020a0e869b0
nerijus-ubuntu <nerijusanz@gmail.com>
2019-01-25 15:32:25 +0200

    new models schemas: Background.js,Characters.js,Race.js

---
+++ models/Race.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  traits: [
+    {
+      name: { type: String, required: true },
+      description: String
+    }
+  ],
+  subraces: [
+    {
+      name: { type: String, required: true },
+      description: String,
+      traits: [
+        {
+          name: { type: String, required: true },
+          description: String
+        }
+      ]
+    }
+  ]
+});
+
+export default mongoose.model("Race", schema);
